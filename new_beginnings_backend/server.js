21f0ea4927f95f12e51276c6a580af944065cef7
arun <arunreddy5018@gmail.com>
2026-04-10 13:53:39 +0530

    fix: Hardcode frontend domain to CORS origin whitelist to bypass client_url check

--- new_beginnings_backend/server.js
+++ new_beginnings_backend/server.js
@@ -29,6 +29,8 @@ app.use(cors({
     process.env.CLIENT_URL || "http://localhost:3000",
     "http://127.0.0.1:5500",   // VS Code Live Server
     "http://localhost:5500",
+    "https://new-beginnings-portal-gbj6.onrender.com",
+    "https://new-beginnings-portal-gbj6.onrender.com/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
